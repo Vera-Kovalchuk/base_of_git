22724389ee5a637f2466c7c94196958120aa2ef6
Vera Kovalchuk <kovalchukvera86@yandex.ru>
2024-09-13 21:32:22 +0400

    add func for clear names

--- src/main.py
+++ src/main.py
@@ -4,12 +4,12 @@ from os.path import split
 def clear_names(file_name:str) -> list:
     """Функция для очистки имен от лишних символов"""
     new_names_list = list()
-    with open('data/' + file_name) as names_file:
+    with open('data/' + file_name, encoding='utf-8') as names_file:
         names_list = names_file.read().split()
         for name_item in names_list:
             new_name = ''
             for symbol in name_item:
-                if symbol.isalfa():
+                if symbol.isalpha():
                     new_name += symbol
             if new_name.isalpha():
                 new_names_list.append(new_name)
